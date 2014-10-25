e6ea272a52b5ef90cff7f948764b74d72d643bee
Ben Hudson <bhudson@getpebble.com>
2014-10-25 16:30:44 -0700

    added new size option

--- server.py
+++ server.py
@@ -1,4 +1,5 @@
 import os
+import re
 import subprocess
 import tempfile
 
@@ -6,14 +7,12 @@ from flask import Flask, redirect, abort
 from flask.ext.restful.reqparse import RequestParser
 from flask.ext.restful import Api, Resource
 
-app = Flask(__name__,
-            static_url_path='/tmp',
-            static_folder='/tmp')
+app = Flask(__name__, static_url_path='/tmp', static_folder='/tmp')
 api = Api(app)
 
 parser = RequestParser()
 parser.add_argument('image', required=True)
-parser.add_argument('fullscreen', default=False)
+parser.add_argument('size', default='144x152')
 parser.add_argument('dither', default='FloydSteinberg')
 
 dithers = ['FloydSteinberg', 'Riemersma']
@@ -25,36 +24,38 @@ class RemoteMagick(Resource):
         original = args['image']
         _, converted = tempfile.mkstemp(suffix='.png')
 
-        if args['fullscreen']:
-            size = '144x168'
-        else:
-            size = '144x152'
+        if re.match('^\d+x\d+$', args['size']) is None:
+            abort(400)
 
         if not args['dither'] in dithers:
             abort(400)
 
         try:
-            subprocess.check_call(['./bin/convert', original,
-                             '-type', 'Grayscale',
-                             '-colorspace', 'Gray',
-                             '-resize', size + '^',
-                             '-gravity', 'center',
-                             '-extent', size,
-                             '-dither', args['dither'],
-                             '-colors', '2',
-                             '-depth', '1',
-                             '-define', 'png:compression-level=9',
-                             '-define', 'png:compression-strategy=0',
-                             '-define', 'png:exclude-chunk=all',
-                             '-define', 'png:bit-depth=1',
-                             '-define', 'png:color-type=0',
-                             'PNG:' + converted])
+            subprocess.check_call(['convert', original,
+                                   '-type', 'Grayscale',
+                                   '-colorspace', 'Gray',
+                                   '-resize', args['size'] + '^',
+                                   '-gravity', 'center',
+                                   '-extent', args['size'],
+                                   '-dither', args['dither'],
+                                   '-colors', '2',
+                                   '-depth', '1',
+                                   '-define', 'png:compression-level=9',
+                                   '-define', 'png:compression-strategy=0',
+                                   '-define', 'png:exclude-chunk=all',
+                                   '-define', 'png:bit-depth=1',
+                                   '-define', 'png:color-type=0',
+                                   'PNG:' + converted])
         except subprocess.CalledProcessError:
             abort(400)
 
-        return redirect(converted);
+        return redirect(converted)
 
 api.add_resource(RemoteMagick, '/api')
+
+@app.route('/')
+def index():
+    return redirect('http://github.com/pebble-hacks/remote-magick')
 
 if __name__ == '__main__':
     port = os.environ.get('PORT', '5000')
